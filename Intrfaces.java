ecd49a07de0161527788a01e0b8b7a2a19fed71a
Talha-shariff <sharifftalha004@gmail.com>
2025-06-17 02:38:42 +0530

    more in depth of implemention of the abstract classes

--- Intrfaces.java
+++ Intrfaces.java
@@ -12,4 +12,34 @@
 //  }
 
 // little depth going in abstract classes
+   abstract class Computer{
+    public abstract void code();
+}
+
+class Laptop extends Computer{
+    public void code(){
+        System.out.println("coding, compilling, and running in laptop");
+    }
+}
+class Desktop extends Computer{
+    public void code(){
+        System.out.println("coding, compilling, and running in PC");
+    }
+}
+class Developer {
+    // here the code method has already inherited the computer class
+    public void code(Computer val){
+        val.code();
+    }
+}
+public class Intrfaces {
+    public static void main(String[] args){
+
+       Computer lap = new Laptop();
+       Computer desk = new Desktop();
+       Developer Talha = new Developer();
+       Talha.code(lap);
+       Talha.code(desk);
+    }
+}
  
